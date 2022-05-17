0bbe58591aa615767da50f7732e6c5067f120297
nk321626 <nk321626@gmail.com>
2022-05-17 21:34:35 +0530

    Create a Binary Search Tree by Adding UC1

--- BinarySerchTree/Program.cs
+++ BinarySerchTree/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Binary Search Tree Program");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Displaying the message
+            Console.WriteLine("Welcome to Binary Search Tree Program");
+            Console.WriteLine();
+            //Creating binary seaRch tree to insret and display node values(UC1)
+            BinSearchTree<int> binaryTree = new BinSearchTree<int>(56);
+            binaryTree.Insert(30);
+            binaryTree.Insert(70);
+            binaryTree.Display();
+            Console.WriteLine("\n===================================================");
+            Console.ReadLine();
+        }
+    }
+}
